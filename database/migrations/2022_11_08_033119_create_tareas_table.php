92f56c8ec39aeebebdae71f87352059f36a03178
FranklinFabian <choque.franky@gmail.com>
2022-11-07 23:51:14 -0400

    Modelo Tarea y Migracion

---
+++ database/migrations/2022_11_08_033119_create_tareas_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTareasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tareas', function (Blueprint $table) {
+            $table->BigIncrements('id');
+            $table->string('nombre');
+            $table->String('descripcion');
+            $table->timestamps();
+        });
+    }
+
+    /**s
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tareas');
+    }
+}
